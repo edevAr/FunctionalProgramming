99240b24b8cf0a5ba56608bf9d8eb62b6221caae
Eden Ariel Guzman Andia <edevar7960@gmail.com>
2020-06-15 13:04:14 -0400

    I have improve the solution

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,26 +4,20 @@ import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String args[]){
-        readWordsInFile(args[0], new HashMap<>());
-    }
-
-    private static void readWordsInFile(String pathFile, Map<String, Integer> map) {
         try{
-            File file = new File(pathFile);
-
-            Scanner s = new Scanner(file);
-            while (s.hasNext()) {
-                map.merge(s.next(), 1, Integer::sum);
-            }
-
-            printElements(map.entrySet().stream()
-                    .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
-                    .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e2,
-                            LinkedHashMap::new)));
-
+            readWordsInFile(new Scanner(new File(args[0])), new HashMap<>());
         }catch (IOException e){
             System.out.println("El archivo no existe o la ruta es invalida, por favor verifique e intente nuevamente");
         }
+    }
+    private static void readWordsInFile(Scanner s, Map<String, Integer> map) {
+        while (s.hasNext()) {
+            map.merge(s.next(), 1, Integer::sum);
+        }
+        printElements(map.entrySet().stream()
+                .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e2,
+                        LinkedHashMap::new)));
     }
     private static void printElements(Map<String, Integer> elements){
         elements.forEach((key,value) -> {
